8efc09f0f77c7c6a418d4c1a36c0d481fbe951ad
kgfitzgerald <kgfitzgerald@u.northwestern.edu>
2023-11-11 20:40:15 -0800

    shiny

---
+++ lesson-02/app.R
@@ -0,0 +1,18 @@
+# follow instructions from Lesson 2 https://shiny.rstudio.com/tutorial/written-tutorial/lesson2/
+# instead of editing your hello-shiny app.R from Lesson 1 (they call this App-1/app.R), 
+# use the starter code below
+
+library(shiny)
+
+# Define UI ----
+ui <- fluidPage(
+  
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
---
+++ lesson-03/app.R
@@ -0,0 +1,3 @@
+# follow instructions from Lesson 3 https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
+# instead of adapting the app from Lesson 2 (they call this App-1/app.R), 
+# copy and paste the Adding Widgets code into this file and work from there
